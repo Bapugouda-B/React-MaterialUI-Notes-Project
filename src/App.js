e2ff5a562eec130ed66dc2c9aa898616e6f79a0b
Bapugouda <rajjdanush@gmail.com>
2023-10-26 15:49:41 +0530

    Explored TextField, Radio, Form MUI components and used it in this project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Notes from "./components/Notes.js";
 import CreateNotes from "./components/CreateNotes.js";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import { purple } from "@material-ui/core/colors";
+
+
+//creating theme (it will help to override the default theme properties)
+const theme = createTheme({
+palette:{
+  primary:{
+    main: "#fefefe"
+  },
+  secondary: purple
+}
+})
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Notes />} />
         <Route path="/create" element={<CreateNotes />} />
       </Routes>
     </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/components/CreateNotes.js
+++ src/components/CreateNotes.js
@@ -1,13 +1,111 @@
-import React from "react";
-import { Typography } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+  Typography,
+  Button,
+  Container,
+  TextField,
+  Radio,
+  RadioGroup,
+  FormControl,
+  FormLabel,
+} from "@material-ui/core";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+import { FormControlLabel, makeStyles } from "@material-ui/core";
+
+// very useful for adding custom styles
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: "block",
+  },
+});
 
 const CreateNotes = () => {
+  const classes = useStyles();
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [descriptionError, setDescriptionError] = useState(false);
+  const [category, setCategory] = useState("todos");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTitleError(false);
+    setDescriptionError(false);
+
+    if (title === "") {
+      setTitleError(true);
+    }
+    if (description === "") {
+      setDescriptionError(true);
+    }
+    if (title && description) {
+      console.log(title, description, category);
+    }
+  };
+
   return (
-    <div>
-      <Typography variant="h6" component="h2" gutterBottom color="textSecondary">
+    <Container>
+      <Typography
+        variant="h6"
+        component="h2"
+        gutterBottom
+        color="textSecondary"
+      >
         Create a New Note
       </Typography>
-    </div>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          className={classes.field}
+          label="Title"
+          color="secondary"
+          variant="filled"
+          fullWidth
+          required
+          error={titleError}
+        />
+        <TextField
+          onChange={(e) => setDescription(e.target.value)}
+          className={classes.field}
+          label="Description"
+          color="secondary"
+          variant="filled"
+          multiline
+          rows={5}
+          fullWidth
+          required
+          error={descriptionError}
+        />
+        <FormControl className={classes.field}>
+          <FormLabel>Note Category</FormLabel>
+          <RadioGroup
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <FormControlLabel value="money" control={<Radio />} label="Money" />
+            <FormControlLabel value="todos" control={<Radio />} label="Todos" />
+            <FormControlLabel
+              value="reminders"
+              control={<Radio />}
+              label="Reminders"
+            />
+            <FormControlLabel value="work" control={<Radio />} label="Work" />
+          </RadioGroup>
+        </FormControl>
+        <Button
+          type="submit"
+          color="secondary"
+          variant="contained"
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Submit
+        </Button>
+      </form>
+    </Container>
   );
 };
 
